1abb636293cdec419bec282424db29c5f648b6e6
cartothemax <cartothemax@gmail.com>
2014-10-21 20:06:24 -0500

    added nextgame end point

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-var express = require('express')
+var express = require('express');
 var app = express();
+var cfcepl = require('./cfcepl.json');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+	response.send(cfcepl.games);
+});
 
-app.listen(app.get('port'), function() {
+app.get('/nextgame', function(request, response) {
+	var gameDate;
+	for (game in cfcepl.games) {
+		var curGame = cfcepl.games[game];
+		gameDate = new Date(curGame.year, curGame.month, curGame.day, curGame.hour, curGame.minute);
+		if (gameDate > new Date()) {
+			response.send(curGame);
+			return;
+		}
+	};
+response.send([]);
+});
+
+app.listen(app.get('port'), function() {	
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
