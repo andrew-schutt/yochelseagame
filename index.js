19f3a705ccffcfb57d2ec49174e80184cfa6ddfb
cartothemax <cartothemax@gmail.com>
2015-01-18 07:48:23 -0600

    added http package to project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
+var http = require('http');
 var app = express();
 
 function dateDifference(older, newer) {
